52f8937a759f3496fb138beed01c571cd240d4cd
kopch1k <106131413+kopch1k@users.noreply.github.com>
2022-05-24 17:57:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ def telegram_bot():
                 markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
                 item1=types.KeyboardButton("news")
                 markup.add(item1)
-                bot.send_message(message.chat.id,'Выберите что вам надо',reply_markup=markup)
+                bot.send_message(message.chat.id,'Choose what you need:',reply_markup=markup)
 
 
 
